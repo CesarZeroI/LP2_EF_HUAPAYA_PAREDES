11cf88120b41de95067350285a3e8a4138aea9f0
CesarZeroI <cesarhuapayaparedes@hotmail.com>
2024-10-23 19:28:21 -0500

    Desarrollo del examen

---
+++ src/main/java/pe/com/cibertec/LP2_EF_HUAPAYA_PAREDES_CESAR/Lp2EfHuapayaParedesCesarApplication.java
@@ -0,0 +1,13 @@
+package pe.com.cibertec.LP2_EF_HUAPAYA_PAREDES_CESAR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lp2EfHuapayaParedesCesarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lp2EfHuapayaParedesCesarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/com/cibertec/LP2_EF_HUAPAYA_PAREDES_CESAR/Lp2EfHuapayaParedesCesarApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.cibertec.LP2_EF_HUAPAYA_PAREDES_CESAR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lp2EfHuapayaParedesCesarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
